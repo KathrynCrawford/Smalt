ded0e373e7333c9219bc3d12bdba75dbfeb6fd34
kathryn crawford <kathryn.crawford@ivieinc.com>
2016-11-08 17:05:13 -0600

    Made some minor changes to nav markup and updated comments and sample markup

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,3 @@
-//PILLS
-
-jQuery(function($) {
-    // this will get the full URL at the address bar
-    var url = window.location.href;
-
-    // passes on every "a" tag
-    $('.nav_link').each(function() {
-        // checks if its the same on the address bar
-        if (url == (this.href)) {
-            $(this).closest('li').addClass('active');
-        }
-    });
-});
-
 //COLLAPSE
 
 /**
